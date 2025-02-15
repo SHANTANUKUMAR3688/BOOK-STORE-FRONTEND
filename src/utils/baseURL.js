ccb95c18a5d5ed58e3de87ec59de57489ba856ff
Your Name <your.email@example.com>
2025-02-15 13:06:39 +0530

    new

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000","https://book-store-backend1-hv5w.onrender.com/"
+    return "https://book-store-backend1-hv5w.onrender.com/"
 }
 
 export default getBaseUrl;
